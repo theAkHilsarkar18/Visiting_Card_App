9775bcbf101a9925185572c8cd025eea06200560
akhil <akhilsodvadiya7@gmail.com>
2024-01-06 16:49:57 +0530

    06-01-2023

--- app/src/main/java/com/example/visitingcardapp/CardScreen.java
+++ app/src/main/java/com/example/visitingcardapp/CardScreen.java
@@ -2,13 +2,47 @@ package com.example.visitingcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CardScreen extends AppCompatActivity {
 
+   TextView txtName;
+    TextView txtBusinessName1;
+    TextView txtBusinessName2;
+    TextView txtPhone;
+    TextView txtEmail;
+    TextView txtAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_screen);
+
+        txtName = findViewById(R.id.cardName);
+        txtBusinessName1 = findViewById(R.id.card_business_name);
+        txtBusinessName2 = findViewById(R.id.card_business_name2);
+        txtPhone = findViewById(R.id.cardPhone);
+        txtAddress = findViewById(R.id.cardAddress);
+        txtEmail = findViewById(R.id.cardEmail);
+
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra("name");
+        String business = intent.getStringExtra("business");
+        String phone = intent.getStringExtra("phone");
+        String email = intent.getStringExtra("email");
+        String address = intent.getStringExtra("address");
+
+        txtName.setText(name);
+        txtEmail.setText(email);
+        txtBusinessName1.setText(business);
+        txtBusinessName2.setText(business);
+        txtPhone.setText(phone);
+        txtAddress.setText(address);
+
+
+
     }
 }
--- app/src/main/java/com/example/visitingcardapp/HomeScreen.java
+++ app/src/main/java/com/example/visitingcardapp/HomeScreen.java
@@ -3,28 +3,94 @@ package com.example.visitingcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
     Button button;
+
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_home_screen);
         button = (Button) findViewById(R.id.button);
+        EditText txtName = findViewById(R.id.name);
+        EditText txtBusinessName = findViewById(R.id.business_name);
+        EditText txtPhone = findViewById(R.id.phone);
+        EditText txtEmail = findViewById(R.id.email);
+        EditText txtAddress = findViewById(R.id.address);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = txtName.getText().toString();
+                String business = txtBusinessName.getText().toString();
+                String phone = txtPhone.getText().toString();
+                String email = txtEmail.getText().toString();
+                String address = txtAddress.getText().toString();
+
+                Intent sendDetail = new Intent(getApplicationContext(), CardScreen.class);
+                sendDetail.putExtra("name",name);
+                sendDetail.putExtra("business",business);
+                sendDetail.putExtra("phone",phone);
+                sendDetail.putExtra("email",email);
+                sendDetail.putExtra("address",address);
                 openNewActivity();
+                Toast.makeText(getBaseContext(), "Detail Submitted !" , Toast.LENGTH_SHORT ).show();
             }
         });
 
+        img = (ImageView) findViewById(R.id.uploadImageButton);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                imageSelect();
+
+            }
+        });
+
+
+
     }
+
+    void imageSelect()
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i, "Select Picture"),SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    img.setImageURI(selectedImageUri);
+                    img.setClipToOutline(true);
+                }
+            }
+        }
+    }
+
+
 
     public void openNewActivity(){
         Intent intent = new Intent(this, CardScreen.class);
